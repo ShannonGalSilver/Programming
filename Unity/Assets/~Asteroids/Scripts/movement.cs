acf31ff4b59dec288e85f03f29714c31df85b6bd
ShannonGalSilver <shannon.c.gallagher@gmail.com>
2018-05-11 12:00:40 +1000

    started movement script in Asteroids

---
+++ Unity/Assets/~Asteroids/Scripts/movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Asteroids
+{
+
+    public class movement : MonoBehaviour
+    {
+        public float speed = 20f;
+        public float rotationSpeed = 360f;
+
+        private Rigidbody2D rigid;
+        
+        // Use this for initialization
+        void Start()
+        {
+            rigid = GetComponent<Rigidbody2D>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Rotate(Vector3.forward, rotationSpeed);
+            }
+        }
+    }
+}
