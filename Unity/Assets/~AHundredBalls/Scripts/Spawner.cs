b44ff518ac79bb1afb7a4e278fe1c126b589cab5
ShannonGalSilver <shannon.c.gallagher@gmail.com>
2018-05-18 11:56:37 +1000

    Started a new game "A Hundred Balls"

---
+++ Unity/Assets/~AHundredBalls/Scripts/Spawner.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace AHundredBalls
+{
+    public class Spawner : MonoBehaviour
+    {
+        public GameObject prefab;
+        public float spawnDelay = 2f;
+        public bool spawnRandomly = false;
+        public float range = 10f;
+
+      void OnDrawGizmos()
+        {
+            if (spawnRandomly)
+            {
+                Vector2 position = transform.position;
+                float halfRange = range * 0.5f;
+                Vector2 start = new Vector2(position.x - halfRange, position.y);
+                Vector2 end = new Vector2(position.x + halfRange, position.y);
+                Gizmos.DrawLine(start, end);
+                               
+            }
+        }
+        void Start()
+        {
+            StartCoroutine(Spawn());
+        }
+
+        IEnumerator Spawn()
+        {
+            yield return new WaitForSeconds(spawnDelay);
+
+            Vector3 position = transform.position;
+            
+            if (spawnRandomly)
+            {
+                float halfRange = range * 0.5f;
+                position.x += Random.Range(-range, range);
+            }
+
+            Instantiate(prefab, position, transform.rotation);
+
+            StartCoroutine(Spawn());
+        }
+    }
+}
