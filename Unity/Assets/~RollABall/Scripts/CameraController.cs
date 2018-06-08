8846c853640eb6d42753088f92d7ec984a3eed4c
ShannonGalSilver <shannon.c.gallagher@gmail.com>
2018-06-08 11:59:32 +1000

    Started new project "RollABall"

---
+++ Unity/Assets/~RollABall/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace RollABall
+{
+    public class CameraController : MonoBehaviour
+    {
+
+        public GameObject player;
+
+        //holds set distance between camera and player
+        private Vector3 offset;
+
+        void Start()
+        {
+            offset = transform.position - player.transform.position;
+        }
+
+        // LateUpdate runs every frame but is guaranteed to run after all items have been processed in update
+        void LateUpdate()
+        {
+            transform.position = player.transform.position + offset;
+        }
+    }
+}
---
+++ Unity/Assets/~RollABall/Scripts/CollectableRotator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableRotator : MonoBehaviour {
+
+
+	void Update () {
+        //indicates how the object rotates and ensures rotation is smooth with deltaTime
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+	}
+}
