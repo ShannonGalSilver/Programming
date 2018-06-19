1de23b9fd368402256c39dcbc41f33e85479e741
ShannonGalSilver <36747320+ShannonGalSilver@users.noreply.github.com>
2018-06-19 13:08:21 +1000

    Added falling 'enemies'

---
+++ Unity/Assets/~Assessment1/Scripts/EnemySpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Assessment1
+{
+    public class EnemySpawner : MonoBehaviour
+    {
+
+        public GameObject[] enemies;
+        public float spawnRate = 1f;
+        public float spawnRadius = 5f;
+
+        void Spawn()
+        {
+            Vector3 rand = Random.insideUnitSphere * spawnRadius;
+
+            rand.z = 0f;
+
+            Vector3 position = transform.position + rand;
+
+            int randIndex = Random.Range(0, enemies.Length);
+
+            GameObject randEnemy = enemies[randIndex];
+
+            GameObject clone = Instantiate(randEnemy);
+
+            clone.transform.position = position;
+        }
+
+        void Start()
+        {
+            InvokeRepeating("Spawn", 0, spawnRate);
+        }
+    }
+}
