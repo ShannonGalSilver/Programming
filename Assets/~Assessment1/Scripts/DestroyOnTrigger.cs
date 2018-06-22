e0135605aa19775b05f0283896895ab64c72bf07
ShannonGalSilver <shannon.c.gallagher@gmail.com>
2018-06-22 10:10:35 +1000

    Fixed Death and added level progression

---
+++ Assets/~Assessment1/Scripts/DestroyOnTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assessment1
+{
+    public class DestroyOnTrigger : MonoBehaviour
+    {
+        public void OnTriggerEnter(Collider other)
+        {
+            Destroy(gameObject);
+
+        }
+    }
+}
---
+++ Assets/~Assessment1/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+namespace Assessment1
+{
+    public class GameManager : MonoBehaviour
+    {
+        // Switch to next level when this function runs
+        public void NextLevel()
+        {
+            // Get active scene
+            Scene currentScene = SceneManager.GetActiveScene();
+            // Load the next scene up using buildIndex
+            SceneManager.LoadScene(currentScene.buildIndex + 1);
+        }
+    }
+}
